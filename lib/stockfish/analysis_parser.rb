bcc1be172210fa036395f55e8ff486a0f89e38be
Linmiao Xu <linmiao.xu@gmail.com>
2015-12-08 16:39:35 -0800

    Update the parser to support multipv analysis outputs

--- lib/stockfish/analysis_parser.rb
+++ lib/stockfish/analysis_parser.rb
@@ -27,32 +27,84 @@ module Stockfish
     end
 
     def parse
+      if game_over?
+        handle_game_over
+        return
+      end
+      if @raw_analysis.include?("multipv")
+        multipv_parse
+      else
+        singlepv_parse
+      end
+    end
+
+    def game_over?
+      !!@raw_analysis[/bestmove \(none\)/]
+    end
+
+    def handle_game_over
       if @raw_analysis[/info depth 0 score mate 0/]
         @analysis = {
           :bestmove => nil,
           :score => "mate 0"
         }
+      elsif @raw_analysis[/info depth 0 score cp 0/]
+        @analysis = {
+          :bestmove => nil,
+          :score => 0
+        }
+      end
+      @analysis
+    end
+
+    def best_move_uci
+      @raw_analysis[/bestmove (\w+)/, 1]
+    end
+
+    def parse_move_row(row)
+      sequence = row.match(/ pv (?<moves>.*)/)
+      return if sequence.nil?
+      analysis = row.match(ROW_SCANNER)
+      score = case analysis[:score_type]
+              when "cp" then analysis[:score].to_f/100
+              when "mate" then "mate #{analysis[:score]}"
+              end
+      output = {
+        :score    => score,
+        :sequence => sequence[:moves],
+        :depth    => analysis[:depth].to_i,
+      }
+      output[:multipv] = analysis[:multipv].to_i if analysis[:multipv]
+      output
+    end
+
+    def singlepv_parse
+      @raw_analysis.strip.split("\n").reverse.each do |row|
+        analysis = parse_move_row(row)
+        next if analysis.nil? || analysis[:sequence].split(" ")[0] != best_move_uci
+        @analysis = {
+          :bestmove => best_move_uci,
+          :moves    => [analysis]
+        }
         return @analysis
       end
-      best_move_uci = @raw_analysis[/bestmove (\w+)/, 1]
+      @analysis || {}
+    end
+
+    def multipv_parse
+      multipv = nil
+      count = 0
+      @analysis = {
+        :bestmove => best_move_uci,
+        :moves    => []
+      }
       @raw_analysis.strip.split("\n").reverse.each do |row|
-        sequence = row.match(/ pv (?<moves>.*)/)
-        next if sequence.nil? || sequence[:moves].split(" ")[0] != best_move_uci
-        analysis = row.match(ROW_SCANNER)
-        score = case analysis[:score_type]
-                when "cp" then analysis[:score].to_f/100
-                when "mate" then "mate #{analysis[:score]}"
-                end
-        depth = analysis[:depth].to_i
-        if score && sequence[:moves] && depth
-          @analysis = {
-            :bestmove => best_move_uci,
-            :sequence => sequence[:moves],
-            :score => score,
-            :depth => depth,
-          }
-          return @analysis
-        end
+        analysis = parse_move_row(row)
+        next if analysis.nil?
+        multipv = analysis[:multipv] if multipv.nil?
+        @analysis[:moves].push analysis
+        count += 1
+        break if count >= multipv
       end
       @analysis || {}
     end
